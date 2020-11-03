7c89e721ee3bf93a0613ae3a9999a0c40e6020c5
Smeekl <vadimpro3@gmail.com>
2020-11-03 16:50:51 +0200

    Update room template

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import "./index.css";
 import Auth from "./components/Auth/Auth";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Room from "./components/Room/Room";
+import socketIOClient from "socket.io-client";
+import Chat from "./components/Chat/Chat";
+
+const ENDPOINT = "http://localhost:3001";
 
 function App() {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path="/" component={Auth} />
-                <Route exact path="/joinRoom" component={Room} />
-            </Switch>
-        </Router>
-    );
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT, { origins: "*:*" });
+    socket.onopen(() => {
+      console.log("123");
+    });
+  });
+
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Auth} />
+        <Route exact path="/room" component={Room} />
+        <Route exact path="/chat" component={Chat} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
